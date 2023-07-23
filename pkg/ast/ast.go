768fd288b09f58c6dc1de8b79567d8f0e5a084e6
hari <harichandmanoj@gmail.com>
2023-07-23 20:16:13 +0530

    initial ast

---
+++ pkg/ast/ast.go
@@ -0,0 +1,28 @@
+package ast
+
+type Node interface {
+	TokenValue() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenValue() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenValue()
+	} else {
+		return " "
+	}
+
+}
